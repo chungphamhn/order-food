9b3d4c445ff843045c31e2a601de8329574dacac
phamvien15 <vien.pham@edu.turkuamk.fi>
2020-09-13 18:11:12 +0300

    button modified

--- src/container/Dishes/SingleDish.js
+++ src/container/Dishes/SingleDish.js
@@ -5,46 +5,81 @@ import './SingleDish.css';
 
 class SingleDish extends Component {
     state = {
-        isClicked: false
+        isClicked: false,
+        count: 0,
+        showAddButton: true
     }
-    clickHandle = () => {
+    showAddButtonHandler = () => {
+        this.setState({showAddButton: !this.state.showAddButton});
+    }
+    
+    addDishHanle = () => {
+        this.setState(state => ({
+            count: state.count + 1
+        }));
+    }
+
+    minusDishHandle = () => {    
+        if (this.state.count > 0){
+            this.setState(state => ({
+                count: state.count - 1
+            }));
+        }
+        else if (this.state.count === 0){
+            this.setState(
+                {
+                    isClicked: !this.state.isClicked,
+                    showAddButton: true
+                });
+         }
+    }
+
+     clickHandle = () => {
         this.setState({ isClicked: !this.state.isClicked });
     }
     render() {
+        let showCount = (
+            <div>
+                <button className = "btn" onClick ={this.showAddButtonHandler}>Add to order</button>
+            </div>
+        );
 
+        if (!this.state.showAddButton){
+            showCount = (
+               <div> count: {this.state.count}
+                    <button onClick = {this.minusDishHandle}>-</button>
+                    <button onClick = {this.addDishHanle}>+</button>
+                </div>
+            );
+        }
         let dish = (
             <div onClick={this.clickHandle} key={this.props.Key} className="SingleDish">
                 <div>
                     <div className="title"><strong>{this.props.dishName}</strong></div>
-                    <div>Price: {this.props.dishPrice} Euro</div>
+                    <div className="price">€{this.props.dishPrice} </div>
                 </div>
-                <img src={this.props.dishPic} alt='pics of burger' />
+                    <img src={this.props.dishPic} alt='pics of burger' />
             </div>
         );
-
-
+       
         if (this.state.isClicked) {
-
             dish = (
-                <div onClick={this.clickHandle} key={this.props.Key} className="SingleDish" >
-                    <div>
+          
+                <div key={this.props.Key} className="SingleDishOpen" >
+                    <div onClick={this.clickHandle}><img src={this.props.dishPic} alt='pics of Nachos' /></div>
+                    <div onClick={this.clickHandle}>
                         <div className="title"><strong>{this.props.dishName}</strong></div>
-                        <div>
-                            count:
-                            <button>-</button>
-                            <button>+</button>
-                        </div>
-
-                        <p>Price: {this.props.dishPrice} Euro</p>
+                        <p className="price">€{this.props.dishPrice}</p>
                     </div>
-                    <img src={this.props.dishPic} alt='pics of Nachos' />
+                    {showCount}
                 </div>
+            
             );
         }
-
         return (
             <div>
                 {dish}
+                
             </div>
 
         );
